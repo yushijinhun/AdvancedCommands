4301492e5aa218866bc2e756899f8b340adf1b65
yushijinhun <yushijinhun@gmail.com>
2015-03-22 20:51:28 +0800

    refactor command feedback

--- src/main/java/yushijinhun/advancedcommands/common/command/BasicCommand.java
+++ src/main/java/yushijinhun/advancedcommands/common/command/BasicCommand.java
@@ -1,8 +1,15 @@
 package yushijinhun.advancedcommands.common.command;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import yushijinhun.advancedcommands.AdvancedCommands;
+import yushijinhun.advancedcommands.Config;
+import yushijinhun.advancedcommands.util.ExceptionHelper;
+import yushijinhun.advancedcommands.util.LocalizationHelper;
 
 public abstract class BasicCommand extends CommandBase {
 
@@ -15,4 +22,31 @@ public abstract class BasicCommand extends CommandBase {
 		}
 		return result;
 	}
+
+	@Override
+	public void execute(ICommandSender sender, String[] args) throws CommandException {
+		try {
+			doExecute(sender, args);
+			if (Config.sendExecutedMessageToOps) {
+				notifyOperators(sender, this, "Command '" + getName() + "' executed " + Arrays.toString(args));
+			}
+		} catch (Throwable e) {
+			if (Config.printErrorMessageToConsole) {
+				AdvancedCommands.logger.info(
+						LocalizationHelper.localizeString("advancedcommands.command.execute.failed", getName(),
+								Arrays.toString(args)), e);
+			}
+
+			if (Config.sendErrorMessageToOps) {
+				notifyOperators(
+						sender,
+						this,
+						LocalizationHelper.localizeString("advancedcommands.command.execute.failed", getName(),
+								Arrays.toString(args)) + "\n" + ExceptionHelper.exceptionToString(e));
+			}
+			throw new CommandException(e.getClass().getName() + ": " + e.getMessage(), new Object[0]);
+		}
+	}
+
+	protected abstract void doExecute(ICommandSender sender, String[] args);
 }
--- src/main/java/yushijinhun/advancedcommands/common/command/CommandExp.java
+++ src/main/java/yushijinhun/advancedcommands/common/command/CommandExp.java
@@ -1,17 +1,12 @@
 package yushijinhun.advancedcommands.common.command;
 
-import java.util.Arrays;
 import java.util.List;
-import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
-import yushijinhun.advancedcommands.AdvancedCommands;
-import yushijinhun.advancedcommands.Config;
 import yushijinhun.advancedcommands.common.command.var.ExpressionHandler;
 import yushijinhun.advancedcommands.common.command.var.Var;
-import yushijinhun.advancedcommands.util.ExceptionHelper;
 import yushijinhun.advancedcommands.util.LocalizationHelper;
 
 public class CommandExp extends BasicCommand {
@@ -32,31 +27,7 @@ public class CommandExp extends BasicCommand {
 	}
 
 	@Override
-	public void execute(ICommandSender sender, String[] args) throws CommandException {
-		try {
-			doExecute(sender, args);
-			if (Config.sendExecutedMessageToOps) {
-				notifyOperators(sender, this, "Command var executed " + Arrays.toString(args));
-			}
-		} catch (Throwable e) {
-			if (Config.printErrorMessageToConsole) {
-				AdvancedCommands.logger.info(
-						LocalizationHelper.localizeString("advancedcommands.command.execute.failed", "exp",
-								Arrays.toString(args)), e);
-			}
-
-			if (Config.sendErrorMessageToOps) {
-				notifyOperators(
-						sender,
-						this,
-						LocalizationHelper.localizeString("advancedcommands.command.execute.failed", "exp",
-								Arrays.toString(args)) + "\n" + ExceptionHelper.exceptionToString(e));
-			}
-			throw new CommandException(e.getMessage(), new Object[0]);
-		}
-	}
-
-	public void doExecute(ICommandSender sender, String[] args) throws Throwable {
+	public void doExecute(ICommandSender sender, String[] args) {
 		if (args.length == 0) {
 			throw new IllegalArgumentException(
 					LocalizationHelper.localizeString("advancedcommands.command.argument.short"));
