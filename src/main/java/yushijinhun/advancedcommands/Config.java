debf492f378ab439d843da4ec4dc9e530fd09328
yushijinhun <yushijinhun@gmail.com>
2015-03-28 15:56:43 +0800

    add props

--- src/main/java/yushijinhun/advancedcommands/Config.java
+++ src/main/java/yushijinhun/advancedcommands/Config.java
@@ -16,6 +16,8 @@ public final class Config {
 	public static boolean sendErrorMessageToOps = false;
 	public static boolean sendExecutedMessageToOps = false;
 	public static boolean printErrorMessageToConsole = false;
+	public static boolean safetyMode = true;
+	public static int safetyTime = 200;
 
 	public static void loadConfig(File file) {
 		config = new Configuration(file);
@@ -31,7 +33,7 @@ public final class Config {
 		// prop.comment =
 		// StatCollector.translateToLocal("config.<name>.description");
 		// prop.setLanguageKey("config.<name>");
-		// <name> = prop.getBoolean();
+		// <name> = prop.get<xxxx>();
 		// configList.add(prop.getName());
 
 		prop = config.get(Configuration.CATEGORY_GENERAL, "sendErrorMessageToOps",
@@ -54,6 +56,22 @@ public final class Config {
 				StatCollector.translateToLocal("config.sendExecutedMessageToOps.description");
 		prop.setLanguageKey("config.sendExecutedMessageToOps");
 		sendExecutedMessageToOps = prop.getBoolean();
+		configList.add(prop.getName());
+
+		prop = config.get(Configuration.CATEGORY_GENERAL, "safetyMode",
+				Config.safetyMode);
+		prop.comment =
+				StatCollector.translateToLocal("config.safetyMode.description");
+		prop.setLanguageKey("config.safetyMode");
+		safetyMode = prop.getBoolean();
+		configList.add(prop.getName());
+
+		prop = config.get(Configuration.CATEGORY_GENERAL, "safetyTime",
+				Config.safetyTime);
+		prop.comment =
+				StatCollector.translateToLocal("config.safetyTime.description");
+		prop.setLanguageKey("config.safetyTime");
+		safetyTime = prop.getInt();
 		configList.add(prop.getName());
 
 		if (config.hasChanged()) {
