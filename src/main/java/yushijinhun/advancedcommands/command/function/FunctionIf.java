4314b2f80ccb20247a55cd1e88bfa6b725a01ce6
yushijinhun <yushijinhun@gmail.com>
2015-04-16 16:47:42 +0800

    fix wrong type checks for if()

--- src/main/java/yushijinhun/advancedcommands/command/function/FunctionIf.java
+++ src/main/java/yushijinhun/advancedcommands/command/function/FunctionIf.java
@@ -11,7 +11,7 @@ public class FunctionIf extends Function {
 	@Override
 	public Var call(Var[] args, FunctionContext context) {
 		throwArgsLengthExceptionIf((args.length < 2) || (args.length > 3));
-		checkType(args, 0, "string");
+		checkType(args, 0, "boolean");
 		if ((Boolean) args[0].getValue()) {
 			ShellHelper.execute(args[1], context);
 		} else {
